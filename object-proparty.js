0dd9b120ad1a9ae8365358dd7c352067e9b600cc
Sontus Chandra Anik <sontuschandraanik.cse@gmail.com>
2021-02-07 06:41:24 +0600

    Map using for loop

---
+++ object-proparty.js
@@ -0,0 +1,18 @@
+const students = [
+    {id: 21, name : "Atik"},
+    {id: 30, name : "Tarak"},
+    {id: 11, name : "Emon"},
+    {id: 20, name : "Rana"},
+    {id: 31, name : "Sani"},
+    {id: 20, name : "Rofiq"}
+]
+
+// for (let i = 0; i < students.length; i++) {
+//     var newStd = [];
+//     const element = students[i]['name'];
+//     // console.log(element);
+//     newStd.push(element);
+//     console.log(newStd);
+
+// }
+
